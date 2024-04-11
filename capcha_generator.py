3661e837011bdcf0870e4f161f32c3d73351f672
Leo <122302575+leo3622@users.noreply.github.com>
2024-04-10 23:09:19 -0400

    add needed functions

---
+++ capcha_generator.py
@@ -0,0 +1,27 @@
+#requirement: pip install captcha
+#             pip install pillow
+#             sudo apt-get install eog
+#import captcha
+from captcha.image import ImageCaptcha
+from PIL import Image
+import random
+#function that generates a captcha to check if the user is a human or a bot
+def generate_captcha():
+    imagine = ImageCaptcha(width = 280, height = 90)
+    #generate a random string of 6 characters
+    random_str = ''.join([random.choice('abcdefghijklmnopqrstuvwxyz') for i in range(6)])
+    #generate the image of the captcha
+    data = imagine.generate(random_str)
+    #save the image of the captcha
+    imagine.write(random_str, 'captcha.png')
+    #open the image
+    image = Image.open('captcha.png')
+    image.show()
+    print(random_str)
+    #get user input
+    user_input = input('Enter the string: ')
+    return True if user_input == random_str else False
+
+#Test the function
+if __name__ == '__main__':
+    print(generate_captcha())
---
+++ login_function.py
@@ -0,0 +1,38 @@
+#requirement: pip install flask
+from flask import Flask, request
+from werkzeug.utils import secure_filename
+import webbrowser
+import os
+import csv
+#function that simulates the company's update csv file
+app = Flask(__name__)
+@app.route('/upload', methods = ['POST'])
+def upload_file():
+    if 'file' not in request.files:
+        return 'No file part in the request.', 400
+    file = request.files['file']
+    if file.filename == '':
+        return 'No selected file.', 400
+    if file and file.filename.endswith('.csv'):
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(os.getcwd(), filename))
+        return 'File uploaded successfully.', 200
+    else:
+        return 'Unsupported file type.', 400
+#function that stimulates the company's login system
+def login(ip):
+    #check if the ip is trusted
+    if is_ip_in(ip):
+        print('Welcome to the company!')
+        webbrowser.open('file://' + os.path.realpath('update_file.html'))
+        app.run(host = '0.0.0.0', port = 8080)
+    else: print('Only partners are allowed to access this feature!')
+#function that check the ip of the user
+def is_ip_in(ip):
+    with open('partner_companies_info.csv', 'r') as file:
+        reader = csv.reader(file)
+        next(reader)
+        return any(ip == row[0] for row in reader)
+#Test the function
+if __name__ == '__main__':
+    login('8.8.4.1')
