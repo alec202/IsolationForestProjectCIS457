481d7701b96ac2e306a32ee2d38a877b54e21cd5
Alec Mirambeau <alecmirambeau@gmail.com>
2024-04-10 20:10:41 -0400

    Created initial files i'll need

---
+++ isolation_forest_function.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import seaborn as sns
+from sklearn.ensemble import IsolationForest
+import os
+
+def trainModel():
+    df = pd.read_csv("dataForTrainingExampleFormat.txt")
+
+if __name__ == "__main__":
+    trainModel()
