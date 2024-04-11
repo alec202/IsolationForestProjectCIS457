21af25da788d401ebd75a60159efae537c676ce2
Dominic <mancinad@mail.gvsu.edu>
2024-04-11 18:50:11 -0400

    made test matching function

---
+++ testMatch.py
@@ -0,0 +1,23 @@
+from matching import matching
+
+result = matching(122.130.01.20)
+result2 = matching(13.133.1.20)
+result3 = matching(123.456.01.20)
+
+#Test1
+if result is not False:
+    print(f"IP address was found at index {result}")
+else:
+    print("IP address not found")
+
+#Test 2
+if result2 is not False:
+    print(f"IP address was found at index {result}")
+else:
+    print("IP address not found")  
+
+#Test 3 
+if result3 is not False:
+    print(f"IP address was found at index {result}")
+else:
+    print("IP address not found")  
