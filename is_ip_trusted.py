879ceeccab347a96486f34478dbf67262b033609
Alec Mirambeau <alecmirambeau@gmail.com>
2024-04-15 17:32:55 -0400

    modified function so it will read -1 as string since a file is prcesed that way

--- is_ip_trusted.py
+++ is_ip_trusted.py
@@ -5,8 +5,8 @@ def is_ip_not_trusted(ip):
         next(reader)
         # return false if it's not trusted
         # return true if it is trusted
-        return any(ip == row[0] and row[5] == -1 for row in reader)
+        return any(ip == row[0] and row[5] == "-1" for row in reader)
 
 # Test the function
 if __name__ == '__main__':
-    print(is_trusted('122.130.01.20'))
+    print(is_ip_not_trusted('122.130.01.20'))
